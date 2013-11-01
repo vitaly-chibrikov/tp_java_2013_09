14179f9baf140a52b60bad46b92212b20196b71f
v.chibrikov <v.chibrikov@tp-demo1.corp.mail.ru>
2013-11-01 22:56:26 +0400

    server scripts

---
+++ L05/assemble.sh
@@ -0,0 +1,2 @@
+mvn compile assembly:single
+cp ./target/L05-1.0-jar-with-dependencies.jar ./
---
+++ L05/start.sh
@@ -0,0 +1,1 @@
+java -classpath L05-1.0-jar-with-dependencies.jar example.Main 8080 > log.out 2>errors.out& 
