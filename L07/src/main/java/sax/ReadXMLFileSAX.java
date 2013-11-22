c838d72bd46d10a0ebd5829ab241018be9558e33
vitaly.chibrikov <vitaly.chibrikov@gmail.com>
2013-11-23 00:46:13 +0400

    L06

---
+++ L07/src/main/java/sax/ReadXMLFileSAX.java
@@ -0,0 +1,27 @@
+package sax;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ReadXMLFileSAX { 
+	public static Object readXML(String xmlFile) {	 
+	    try {	 
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+		 
+			//SaxHandler handler = new SaxHandler();	
+			SaxEmptyHandler handler = new SaxEmptyHandler();	
+	        saxParser.parse(xmlFile, handler);
+	        
+	        return handler.getObject();
+	 
+	     } catch (Exception e) {
+	       e.printStackTrace();
+	     }
+	    return null;
+	 
+	   }
+	 
+	
+}
